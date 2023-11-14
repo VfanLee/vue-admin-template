0678975112f2c0e1517ec54cb7247974ea77bd80
VfanLee <fanfanfafafa@gmail.com>
2023-11-14 10:18:48 +0800

    mock

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import { viteMockServe } from 'vite-plugin-mock'
 
 // https://cn.vitejs.dev/config/
-export default defineConfig(command => ({
+export default defineConfig(({ command, mode }) => ({
   base: '/vue3-admin-template/',
   plugins: [
     vue(),
@@ -28,11 +28,11 @@ export default defineConfig(command => ({
     // 参考：https://github.com/vbenjs/vite-plugin-mock/tree/v2.9.1
     viteMockServe({
       mockPath: 'mock',
-      localEnabled: command.command === 'serve', // 开发打包开关
-      prodEnabled: command.command !== 'serve', // 生产打包开关
-      supportTs: false, // 打开后，可以读取 ts 文件模块。 请注意，打开后将无法监视.js 文件。
+      localEnabled: command === 'serve',
+      prodEnabled: command !== 'serve',
+      supportTs: false, // 打开后将无法监视 .js 文件
       watchFiles: true, // 监视文件更改
-      //这样可以控制关闭mock的时候不让mock打包到最终代码内
+      // 如果在生产环境中开启了 mock 函数，即 prodEnabled=true，则代码会注入到 injectFile 对应的文件底部。默认值为 main.{ts,js}
       injectCode: `
         import { setupProdMockServer } from '../mock';
         setupProdMockServer();
